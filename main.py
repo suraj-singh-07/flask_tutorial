b866069d62c8bd28ef3d0c42b48199fe13f2338a
suraj-singh-07 <iam.surajsingh07@gmail.com>
2021-02-15 23:15:41 +0530

    third commit

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'ffeddf85654b31436de092b7c577fe31'
 
 posts = [
     {
@@ -24,6 +28,25 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html',title='About')
+
+@app.route("/register",methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!','success')
+        return redirect(url_for('home'))
+    return render_template('register.html',title='Register',form=form)
+
+@app.route("/login",methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have beeb logged in!','success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful, Please Check email and password','danger')
+    return render_template('login.html',title='Login',form=form)
 
 # To run application dircet from python without setting environment variable
 if __name__ == '__main__':
